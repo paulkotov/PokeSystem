ff61fd1f40f438ed28858667fd06a60b6bd97544
Paul Kotov <paulkotov1@gmail.com>
2018-04-02 18:01:00 +0500

    fixed error

--- src/components/PokeItem.js
+++ src/components/PokeItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classNames';
+import classNames from 'classnames';
 import styled from 'styled-components';
 import { Button } from 'antd';
 
